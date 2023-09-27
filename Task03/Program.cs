a9db277da6ea16aa3e07de2f863a7a49884b70b8
AnnieSever <sweetmonkey313@outlook.com>
2023-09-27 17:31:49 +1100

    Taks02 rename

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,14 +1,41 @@
-﻿// Напишите программу, 
-// 1. которая на вход принимает одно число (N), 
-// 2. а на выходе показывает все целые числа в промежутке от -N до N.
-// 4 -> "-4,-3,-2,-1,0,1,2,3,4"
-// 2 -> "-2,-1,0,1,2"
+﻿// Напишите программу,
+// 1. которая будет выдавать название дня недели 
+// 2. по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
 
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int count = -n;
-while(count <= n)
+string dayNumber;
+Console.WriteLine("Введите номер дня недели: ");
+dayNumber = Console.ReadLine();
+if (dayNumber == "1")
 {
-    Console.Write($"{count} ");
-    count = count + 1; // count++;
+    Console.WriteLine($"{dayNumber} -> Понедельник");
+} 
+else if (dayNumber == "2")
+{
+    Console.WriteLine($"{dayNumber} -> Вторник");
+} 
+else if (dayNumber == "3")
+{
+    Console.WriteLine($"{dayNumber} -> Среда");
+}
+else if (dayNumber == "4")
+{
+    Console.WriteLine($"{dayNumber} -> Четверг");
+}
+else if (dayNumber == "5")
+{
+    Console.WriteLine($"{dayNumber} -> Пятница");
+} 
+else if (dayNumber == "6")
+{
+    Console.WriteLine($"{dayNumber} -> Суббота");
+} 
+else if (dayNumber == "7")
+{
+    Console.WriteLine($"{dayNumber} -> Воскресенье");
+} 
+else
+{
+    Console.WriteLine("Ошибка ввода!");
 }
